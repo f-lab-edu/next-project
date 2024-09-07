723c6fd315f34c32175dc7e7571d3cae1c72a806
ag502 <ag502@naver.com>
2024-09-07 12:08:33 +0900

    feat: upcoming-movie-list api를 react-query로 랩핑

--- apps/web/src/entities/movie-list/api/movie-list.queries.ts
+++ apps/web/src/entities/movie-list/api/movie-list.queries.ts
@@ -1,12 +1,14 @@
 import { queryOptions } from "@tanstack/react-query";
 
 import { MovieList } from "../model";
+import { UpcomingMovieList } from "../model/upcoming-movie-list";
 
 import MovieListApi from "./movie-list-api";
 import { MovieListReqParams } from "./request-types";
 
 export const movieListQueryKeys = {
   popularMovieList: () => ["popular-movie-list"] as const,
+  upcomingMovieList: () => ["upcoming-movie-list"] as const,
 };
 
 export const movieListQueries = {
@@ -15,5 +17,11 @@ export const movieListQueries = {
       queryKey: [...movieListQueryKeys.popularMovieList(), { page, language, region }],
       queryFn: () => MovieListApi.getPopularMovieList({ page, language, region }),
       select: (data) => new MovieList(data),
+    }),
+  upcomingMovieList: ({ page, language, region }: MovieListReqParams = { page: 1, language: "ko-KR", region: "KR" }) =>
+    queryOptions({
+      queryKey: [...movieListQueryKeys.upcomingMovieList(), { page, language, region }],
+      queryFn: () => MovieListApi.getUpcomingMovieList({ page, language, region }),
+      select: (data) => new UpcomingMovieList(data),
     }),
 };
