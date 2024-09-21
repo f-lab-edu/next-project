adcc12a876820419acf80716d5670e2c1c3c957a
ag502 <ag502@naver.com>
2024-09-21 21:43:00 +0900

    feat: genres model 추가

---
+++ apps/web/src/entities/genres/models/genres.ts
@@ -0,0 +1,15 @@
+import { createBaseModel } from "@/shared/lib";
+
+import { GenresDTO } from "../api";
+
+export class Genres extends createBaseModel<GenresDTO>() {
+  constructor(private genresList: GenresDTO) {
+    super(genresList);
+  }
+
+  get GenresList() {
+    return this.genresList.genres.reduce((prev, { id, name }) => {
+      return prev.set(id, { id, name });
+    }, new Map<number, GenresDTO["genres"][0]>());
+  }
+}
