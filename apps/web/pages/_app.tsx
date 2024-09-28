8d9fd49b5452fd4dcf0443f88a32e7272b5ceca3
ag502 <ag502@naver.com>
2024-09-29 01:46:00 +0900

    fix: query-client instance shared layer로 분리

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,11 +1,11 @@
 import "@/shared/styles/normalize.css";
 import "@/shared/styles/globals.css";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
 
-import { lightThemeClass } from "@/shared/styles";
+import { queryClient as queryClientInstance } from "@/shared/api";
 
 import type { AppProps } from "next/app";
 
@@ -13,14 +13,12 @@ import "swiper/css";
 
 export default function App({ Component, pageProps }: AppProps) {
   // eslint-disable-next-line react/hook-use-state
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(() => queryClientInstance);
 
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
-      <div className={lightThemeClass} id="__app">
-        <Component {...pageProps} />
-      </div>
+      <Component {...pageProps} />
     </QueryClientProvider>
   );
 }
