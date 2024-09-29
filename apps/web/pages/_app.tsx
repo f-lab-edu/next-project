98c3167171fa18825184c1a8db2c86bc207d1177
ag502 <ag502@naver.com>
2024-09-29 12:36:27 +0900

    feat: now-playing-movie-card 컴포넌트 추가

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -6,6 +6,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
 
 import { queryClient as queryClientInstance } from "@/shared/api";
+import { lightThemeClass } from "@/shared/styles";
 
 import type { AppProps } from "next/app";
 
@@ -18,7 +19,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
-      <Component {...pageProps} />
+      <div className={lightThemeClass}>
+        <Component {...pageProps} />
+      </div>
     </QueryClientProvider>
   );
 }
