2ea5aeefdc15561993124b2239ab221cd4209e62
ag502 <ag502@naver.com>
2024-10-06 18:58:52 +0900

    feat: review-author 모델 추가

--- apps/web/src/entities/reviews/models/index.ts
+++ apps/web/src/entities/reviews/models/index.ts
@@ -1,2 +1,3 @@
 export * from "./now-playing-movie-list-reviews";
 export * from "./now-playing-movie-review";
+export * from "./review-author";
--- apps/web/src/entities/reviews/models/now-playing-movie-review.ts
+++ apps/web/src/entities/reviews/models/now-playing-movie-review.ts
@@ -1,4 +1,6 @@
-import { NowPlayingMovieReview as TNowPlayingMovieReview } from "../api";
+import { ReviewAuthorDetails, NowPlayingMovieReview as TNowPlayingMovieReview } from "../api";
+
+import { ReviewAuthor } from "./review-author";
 
 export class NowPlayingMovieReview {
   movieId: number;
@@ -9,6 +11,7 @@ export class NowPlayingMovieReview {
   createdAt: string;
   updatedAt: string;
   reviewUrl: string;
+  authorDetails: ReviewAuthorDetails;
 
   constructor(data: TNowPlayingMovieReview) {
     this.movieId = data.movieId;
@@ -19,5 +22,10 @@ export class NowPlayingMovieReview {
     this.createdAt = data.createdAt;
     this.updatedAt = data.updatedAt;
     this.reviewUrl = data.url;
+    this.authorDetails = data.authorDetails;
+  }
+
+  get AuthorDetails() {
+    return new ReviewAuthor(this.authorDetails);
   }
 }
