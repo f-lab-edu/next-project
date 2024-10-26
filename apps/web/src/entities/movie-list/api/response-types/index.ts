adfa9ab26fdcb0de17831e72e997905b07c68208
ag502 <ag502@naver.com>
2024-10-26 18:54:49 +0900

    feat: upcoming-movie-list server response 타입 추가

--- apps/web/src/entities/movie-list/api/response-types/index.ts
+++ apps/web/src/entities/movie-list/api/response-types/index.ts
@@ -1,5 +1,11 @@
+// client
 export * from "./movie-list";
 export * from "./movie-list-item";
+export * from "./popular-movie-list";
+export * from "./upcoming-movie-list";
+
+// server
 export * from "./movie-list-item.server";
 export * from "./popular-movie-list.server";
 export * from "./popular-movie-list.server";
+export * from "./upcoming-movie-list.server";
