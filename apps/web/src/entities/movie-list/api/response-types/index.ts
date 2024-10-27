6178cf7e3c8a24d568a6a08188e3c4d10817e582
ag502 <ag502@naver.com>
2024-10-27 17:57:34 +0900

    feat: now-playing-movie-list server response 타입 추가

--- apps/web/src/entities/movie-list/api/response-types/index.ts
+++ apps/web/src/entities/movie-list/api/response-types/index.ts
@@ -3,9 +3,11 @@ export * from "./movie-list";
 export * from "./movie-list-item";
 export * from "./popular-movie-list";
 export * from "./upcoming-movie-list";
+export * from "./now-playing-movie-list";
 
 // server
 export * from "./movie-list-item.server";
 export * from "./popular-movie-list.server";
 export * from "./popular-movie-list.server";
 export * from "./upcoming-movie-list.server";
+export * from "./now-playing-movie-list.server";
