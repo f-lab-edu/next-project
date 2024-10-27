1a73e7c67463c7beece2681a7d1cffe790f74135
ag502 <ag502@naver.com>
2024-10-27 19:44:06 +0900

    fix: movie-list response 타입 프로퍼티 수정

--- apps/web/src/entities/movie-list/api/response-types/movie-list.server.ts
+++ apps/web/src/entities/movie-list/api/response-types/movie-list.server.ts
@@ -2,7 +2,7 @@ import { MovieListItem } from "./movie-list-item.server";
 
 export interface MovieList {
   page: number;
-  result: MovieListItem[];
+  results: MovieListItem[];
   total_pages: number;
   total_result: number;
 }
