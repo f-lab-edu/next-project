61958ef821a2c5309d167351bd226c4b480b0ef4
ag502 <ag502@naver.com>
2024-11-02 19:41:41 +0900

    chore: global theme radius, spacing, typography 디자인 토큰에 tsdocs 추가

--- apps/web/src/shared/styles/tokens/typography.css.ts
+++ apps/web/src/shared/styles/tokens/typography.css.ts
@@ -23,24 +23,72 @@ export const fonts = {
 };
 
 export const fontSizes = {
+  /**
+   * @description `0.75rem` // `16 * 0.75 = 12px`
+   */
   xs: "0.75rem",
+  /**
+   * @description `0.875rem` // `16 * 0.875 = 14px`
+   */
   sm: "0.875rem",
+  /**
+   * @description `1rem` // `16 * 1 = 16px`
+   */
   md: "1rem",
+  /**
+   * @description `1.125rem` // `16 * 1.125 = 20px`
+   */
   lg: "1.125rem",
+  /**
+   * @description `1.25rem` // `16 * 1.25 = 20px`
+   */
   xl: "1.25rem",
+  /**
+   * @description `1.5rem` // `16 * 1.5 = 24px`
+   */
   "2xl": "1.5rem",
+  /**
+   * @description `1.875rem` // `16 * 1.875 = 30px`
+   */
   "3xl": "1.875rem",
+  /**
+   * @description `2.25rem` // `16 * 2.25 = 36px`
+   */
   "4xl": "2.25rem",
+  /**
+   * @description `3rem` // `16 * 3 = 48px`
+   */
   "5xl": "3rem",
+  /**
+   * @description `3.75rem` // `16 * 3.375 = 54px`
+   */
   "6xl": "3.75rem",
+  /**
+   * @description `4.5rem` // `16 * 4.5 = 72px`
+   */
   "7xl": "4.5rem",
+  /**
+   * @description `6rem` // `16 * 6 = 96px`
+   */
   "8xl": "6rem",
+  /**
+   * @description `8rem` // `16 * 8 = 128px`
+   */
   "9xl": "8rem",
 };
 
 export const fontWeights = {
+  /**
+   * @description `400`
+   */
   regular: "400",
+  /**
+   * @description `500`
+   */
   medium: "500",
+  /**
+   * @description `700`
+   */
   bold: "700",
 };
 
