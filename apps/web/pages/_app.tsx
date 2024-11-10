c2d8d024a576aec1568474866c98f4b928813450
ag502 <ag502@naver.com>
2024-11-10 20:50:17 +0900

    feat: page per layout 적용되도록 custom app에 코드 처리

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -3,7 +3,8 @@ import "@/shared/styles/globals.css";
 
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { useState } from "react";
+import { NextPage } from "next";
+import { ReactElement, ReactNode, useState } from "react";
 
 import { queryClient as queryClientInstance } from "@/shared/api";
 import { lightThemeClass } from "@/shared/styles";
@@ -12,16 +13,24 @@ import type { AppProps } from "next/app";
 
 import "swiper/css";
 
-export default function App({ Component, pageProps }: AppProps) {
+export type NextPageWithLayout<P = NonNullable<unknown>, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
   // eslint-disable-next-line react/hook-use-state
   const [queryClient] = useState(() => queryClientInstance);
+
+  const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
-      <div className={lightThemeClass}>
-        <Component {...pageProps} />
-      </div>
+      <div className={lightThemeClass}>{getLayout(<Component {...pageProps} />)}</div>
     </QueryClientProvider>
   );
 }
